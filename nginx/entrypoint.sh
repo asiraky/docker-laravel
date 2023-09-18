3f09d52b37466397012577e555fb8af7eb5c3a36
Aaron HS <asiraky@gmail.com>
2023-09-18 11:25:17 +1000

    escape & in entrypoint

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -16,17 +16,17 @@ server {
     client_max_body_size $CLIENT_MAX_BODY_SIZE;
 
     location / {
-        try_files $uri $uri/ /index.php$is_args$args;
+        try_files \$uri \$uri/ /index.php\$is_args\$args;
     }
 
     location ~ \.php$ {
-        try_files $uri /index.php =404;
+        try_files \$uri /index.php =404;
         fastcgi_pass $PHP_FPM_HOST;
         fastcgi_buffers 16 16k;
         fastcgi_buffer_size 32k;
         fastcgi_index index.php;
         include fastcgi_params;
-        fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
     }
 
     location ~ /\.ht { 
